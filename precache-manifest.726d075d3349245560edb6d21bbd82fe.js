e2d2a38bc4da17a87ea95e0d40a8f213480fa1d5
Frankluisravlo@gmail.com <Frankluisravlo@gmail.com>
2020-07-27 16:34:42 -0400

    Updates

---
+++ precache-manifest.726d075d3349245560edb6d21bbd82fe.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bf57673ad5039c102e98066b307e4b09",
+    "url": "/selfie-mirror/index.html"
+  },
+  {
+    "revision": "a2fa852a2dbea94da694",
+    "url": "/selfie-mirror/static/css/main.5243e8a8.chunk.css"
+  },
+  {
+    "revision": "a6a29cc8e91bb7ca673a",
+    "url": "/selfie-mirror/static/js/2.3aacac04.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/selfie-mirror/static/js/2.3aacac04.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a2fa852a2dbea94da694",
+    "url": "/selfie-mirror/static/js/main.de58a748.chunk.js"
+  },
+  {
+    "revision": "8803d336eb00c9ba04a0",
+    "url": "/selfie-mirror/static/js/runtime-main.c0fa1af9.js"
+  }
+]);
